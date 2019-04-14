470474880c7fa6783c44a6b782e39bfdbb426eae
diqobao <leshy7@gmail.com>
2019-04-13 22:04:31 -0400

    rebase initial commit

--- bookyardApp/auth.py
+++ bookyardApp/auth.py
@@ -3,9 +3,9 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-# from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 #
-# from bookyardApp.db import get_db
+from bookyardApp.db import get_db
 
 authbp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -16,24 +16,24 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        # db = get_db()
+        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        # elif db.execute(
-        #     'SELECT id FROM user WHERE username = ?', (username,)
-        # ).fetchone() is not None:
-        #     error = 'User {} is already registered.'.format(username)
-        #
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
         if error is None:
-            # db.execute(
-            #     'INSERT INTO user (username, password) VALUES (?, ?)',
-            #     (username, generate_password_hash(password))
-            # )
-            # db.commit()
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
             return redirect(url_for('views.index'))
 
         flash(error)
@@ -41,10 +41,30 @@ def register():
     return render_template('register.html')
 
 # Login Page
-@authbp.route('/login')
+@authbp.route('/login', methods=('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-    return 'login'
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('views.index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
 
 # Logout Page
 @authbp.route('/logout')
