ed393df8ce4d4f620da11e4d8f642dc87448f4e2
diqobao <leshy7@gmail.com>
2019-04-16 21:11:28 -0400

    database ini

--- bookyardApp/db.py
+++ bookyardApp/db.py
@@ -1,5 +1,4 @@
-import sqlite3
-import click
+import os, sys, csv, sqlite3, click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
@@ -7,6 +6,16 @@ def init_db():
     db = get_db()
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('iso-8859-1'))
+    reader = csv.reader(open('BX-Books.csv', 'r', encoding='iso-8859-1'),delimiter='\n')
+    count = 0
+    for row in reader:
+        count += 1
+        if count == 1:
+            continue
+        row1 = row[0].split(';"')
+        to_db = [row1[0], row1[1][0:-1], row1[2][0:-1], row1[3][0:-1], row1[4][0:-1], row1[5][0:-1], row1[6][0:-1], row1[7][0:-1]]
+        db.execute('INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES (?, ?, ?,?,?,?,?,?);',to_db)
+    db.commit()
 
 @click.command('init-db')
 @with_appcontext
--- bookyardApp/schema.sql
+++ bookyardApp/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE user (
 CREATE TABLE book (
   bookId INTEGER PRIMARY KEY AUTOINCREMENT,
   isbn TEXT UNIQUE NOT NULL,
-  title TEXT UNIQUE NOT NULL,
+  title TEXT NOT NULL,
   author TEXT,
   year_of_pub INTEGER,
   publisher TEXT,
@@ -26,8 +26,8 @@ CREATE TABLE rating (
   rating INTEGER
 );
 
-INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0195153448','Classical Mythology','Mark P. O. Morford',2002,'Oxford University Press','http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg');
-INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0002005018','Clara Callan','Richard Bruce Wright',2001,'HarperFlamingo Canada','http://images.amazon.com/images/P/0002005018.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0002005018.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0002005018.01.LZZZZZZZ.jpg');
-INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0060973129','Decision in Normandy','Carlo D''Este',1991,'HarperPerennial','http://images.amazon.com/images/P/0060973129.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0060973129.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0060973129.01.LZZZZZZZ.jpg');
-INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0374157065','Flu: The Story of the Great Influenza Pandemic of 1918 and the Search for the Virus That Caused It','Gina Bari Kolata',1999,'Farrar Straus Giroux','http://images.amazon.com/images/P/0374157065.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0374157065.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0374157065.01.LZZZZZZZ.jpg');
+-- INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0195153448','Classical Mythology','Mark P. O. Morford',2002,'Oxford University Press','http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg');
+-- INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0002005018','Clara Callan','Richard Bruce Wright',2001,'HarperFlamingo Canada','http://images.amazon.com/images/P/0002005018.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0002005018.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0002005018.01.LZZZZZZZ.jpg');
+-- INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0060973129','Decision in Normandy','Carlo D''Este',1991,'HarperPerennial','http://images.amazon.com/images/P/0060973129.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0060973129.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0060973129.01.LZZZZZZZ.jpg');
+-- INSERT INTO book (isbn,title,author,year_of_pub,publisher,img_url_s,img_url_m,img_url_l) VALUES ('0374157065','Flu: The Story of the Great Influenza Pandemic of 1918 and the Search for the Virus That Caused It','Gina Bari Kolata',1999,'Farrar Straus Giroux','http://images.amazon.com/images/P/0374157065.01.THUMBZZZ.jpg','http://images.amazon.com/images/P/0374157065.01.MZZZZZZZ.jpg','http://images.amazon.com/images/P/0374157065.01.LZZZZZZZ.jpg');
 
